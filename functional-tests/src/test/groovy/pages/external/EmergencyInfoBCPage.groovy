93c712455108b390bc3258efc9be3b9a76e713b4
George Walker <george@walkersoftware.ca>
2018-02-06 11:33:14 -0800

    Update page title for EmergencyInfo BC

--- functional-tests/src/test/groovy/pages/external/EmergencyInfoBCPage.groovy
+++ functional-tests/src/test/groovy/pages/external/EmergencyInfoBCPage.groovy
@@ -4,6 +4,6 @@ import geb.Page
 
 class EmergencyInfoBCPage extends Page {
 
-	static at = { title == "EmergencyInfoBC" }
+	static at = { title == "Emergency Info BC" }
     static url = "https://www.emergencyinfobc.gov.bc.ca/"	
 }
